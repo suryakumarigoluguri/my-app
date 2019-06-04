d390f81965b2c4455b3e7f4809ba40cbf846f062
Surya Kumari <suryakumarigoluguri>
2019-06-04 15:40:09 +0530

    initial commit

---
+++ build/precache-manifest.7abd1820b1034cc463d309a8a2bfbca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03cdd6b556e8f798b90c36b463e3633f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "367c90eb479877d2e448",
+    "url": "/my-app/static/css/main.3da8c060.chunk.css"
+  },
+  {
+    "revision": "cdde7faea975e2656cfa",
+    "url": "/my-app/static/js/2.2e27ac40.chunk.js"
+  },
+  {
+    "revision": "367c90eb479877d2e448",
+    "url": "/my-app/static/js/main.556e57a8.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
